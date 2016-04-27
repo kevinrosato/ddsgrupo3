5a451e95ddbcbdad34fc2c68ab68ae1c4c820119
bladi <bladimirpari@gmail.com>
2016-04-27 11:45:51 -0300

    agregando getter y setter de latitud

--- src/main/java/ddsgrupo3/POI.java
+++ src/main/java/ddsgrupo3/POI.java
@@ -2,4 +2,21 @@ package ddsgrupo3;
 
 public class POI {
 
+	private String barrio;
+	private String calle;
+	private String localidad;
+	private int altura;
+	private int latitud;
+	private int longitud;
+	
+	
+	public void setLatitud(int platitud) {
+		this.latitud = platitud;
+		
+	}
+
+	public int getLatitud(){
+		return latitud;
+	}
+
 }
