4257bcb146b7f427e16fccb22084dff5c6131b79
bladi <bladimirpari@gmail.com>
2016-04-27 20:50:58 -0300

    primer Test probando latitud de un Poi

--- src/test/java/ddsgrupo3/TestsEntrega0B.java
+++ src/test/java/ddsgrupo3/TestsEntrega0B.java
@@ -1,5 +1,25 @@
 package ddsgrupo3;
 
-public class TestsEntrega0B {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class TestsEntrega0B {
+	POI poi;
+	
+	@Before
+	public void init(){
+		poi = new POI();
+		poi.setLatitud(150);
+		
+	}
+	
+	
+	@Test
+	public void tieneLatitud() {
+		Assert.assertEquals(150,poi.getLatitud());
+		
+	}
+	
+	
 }
