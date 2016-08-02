e43778164bc28f6d0c250709f1fba57d636df25b
kevinrosato <utnkevinrosato@gmail.com>
2016-08-02 16:12:36 -0300

    Archivo de configuracion implementado + cambios en implementacion

--- src/main/java/dds/grupo3/Interfaces/Funcionalidad.java
+++ src/main/java/dds/grupo3/Interfaces/Funcionalidad.java
@@ -3,7 +3,6 @@ package dds.grupo3.Interfaces;
 import java.util.List;
 
 public interface Funcionalidad 
-//TODO: Cambiar el nombre de realizar funcion con POI ya que puede ser otro tipo de funcionalidades
 //TODO:List POIGral deberia ser List Object
 {
 	public Integer	desplegarConsola(User usuario, String terminal_ID);
