dfdfaa7ac8b1d5d33497130fce85860ce34d740d
kevinrosato <utnkevinrosato@gmail.com>
2016-08-02 21:29:01 -0300

    Baja poi asincronica logica terminada

--- src/main/java/dds/grupo3/ProcesosAsincronicos/BajaDePOI.java
+++ src/main/java/dds/grupo3/ProcesosAsincronicos/BajaDePOI.java
@@ -1,9 +1,11 @@
 package dds.grupo3.ProcesosAsincronicos;
 import java.io.FileInputStream;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
+import dds.grupo3.Interfaces.AdministradorPOIs;
 import dds.grupo3.Interfaces.POIGral;
 import dds.grupo3.Interfaces.ProcesoAsincronico;
 import dds.grupo3.Interfaces.User;
@@ -11,22 +13,22 @@ import dds.grupo3.User.BorrarPOI;
 import ddsgrupo3.Mapa;
 
 public class BajaDePOI extends ProcesoAsincronico{
-	private List<POIGral> poi;
-	private Mapa mapa=new Mapa();
+	private List<POIGral> listaPois;
+	private AdministradorPOIs mapa=new Mapa();
 	Boolean error=false;
 	
-	public BajaDePOI(Mapa mapa){
+	public BajaDePOI(AdministradorPOIs mapa){
 		this.mapa=mapa;
 	}
 	
 	@Override
 	public void run(){
 		error=false;
-		poi=llenarListaPOI();
+		listaPois=llenarListaPOI();
 		if (!error){
 			try{
-				for(POIGral i:poi){
-					mapa.realizarFuncConPoi(new BorrarPOI(), i);
+				for(POIGral poi:listaPois){
+					mapa.realizarFuncConPoi(new BorrarPOI(), poi);
 				}
 				this.resultadoOK();
 			}
@@ -43,16 +45,38 @@ public class BajaDePOI extends ProcesoAsincronico{
 			Properties propiedades = new Properties();
 			propiedades.load(file);
 			String respuesta = propiedades.getProperty("RespuestaRESTSimulada");
-			//TODO:Todavia no toma las cosas del archivo
+			String [] respuestaVector= respuesta.split(",");
+			for (int i=0;i<respuestaVector.length;i++){
+				Calendar fechaRespuesta= parseCalendar(respuestaVector[i].split(";")[1]);
+				if(Calendar.getInstance().before(fechaRespuesta)){ //Si el dia de hoy es menor al dia del archivo
+					for(int y=0;y<mapa.getListaPois().size();y++){
+						if(mapa.getListaPois().get(y).tieneLaClave(respuestaVector[i].split(";")[0])){
+							POIGral poi=mapa.getListaPois().get(y);
+							listaPois.add(poi);
+							break;
+						}
+					}
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			error=true;
 			this.resultadoError(e.toString());
 		}
-		return null;
+		return listaPois;
+	}
+	public Calendar parseCalendar (String a){
+		//Pasa de dd/mm/yyyy a Calendar
+		Calendar resultado= Calendar.getInstance();
+		String [] fechaVector= a.split("/");
+		resultado.set(Calendar.YEAR, Integer.parseInt(fechaVector[2]));
+		resultado.set(Calendar.MONTH, Integer.parseInt(fechaVector[1])-1);
+		resultado.set(Calendar.DAY_OF_MONTH, Integer.parseInt(fechaVector[0]));
+		return resultado;
 	}
 	@Override
 	public Integer desplegarConsola(User usuario, String terminal_ID,Scanner teclado) {
+		mapa=usuario.getMapa();
 		usuario.bajaDePOI(terminal_ID);
 		return null;
 	}
