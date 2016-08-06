0417aac5384fb882d5db2faf992f45a06a66c69c
nicky tobal <nicky tobal>
2016-08-06 11:56:22 -0300

    Se implementa patron command

--- src/main/java/dds/grupo3/ProcesosAsincronicos/ActualizacionLocalComercial.java
+++ src/main/java/dds/grupo3/ProcesosAsincronicos/ActualizacionLocalComercial.java
@@ -7,21 +7,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Scanner;
 import java.util.TimerTask;
 
 import dds.grupo3.Interfaces.AdministradorPOIs;
-import dds.grupo3.Interfaces.ProcesoAsincronico;
+import dds.grupo3.Interfaces.CommandProcesos;
 import dds.grupo3.Interfaces.User;
 import dds.grupo3.POIsSistem.Local;
 
-public class ActualizacionLocalComercial extends ProcesoAsincronico{
-	private String nombreLocal;
-	private String palabrasClaves;
-
+public class ActualizacionLocalComercial extends TimerTask implements CommandProcesos{
+	private User usuario;
+	private Calendar fechaInicio;
+	
 	@Override
 	public void run(){
+		ResultadosProcesos resultado=new ResultadosProcesos(this,this.fechaInicio);
 		AdministradorPOIs mapa=usuario.getMapa();
+		String nombreLocal;
+		String palabrasClaves;
 		Boolean tienePOI=false;
 		Local localModificado = null;
 		Integer indice=0;
@@ -34,8 +36,8 @@ public class ActualizacionLocalComercial extends ProcesoAsincronico{
 		try{
 			for(int j=0;j<actualizaciones.size();j++){
 				String[] lista=actualizaciones.get(j).split(";");
-				this.nombreLocal=lista[0];
-				this.palabrasClaves=lista[1];
+				nombreLocal=lista[0];
+				palabrasClaves=lista[1];
 				for(int i=0;i<mapa.getListaPois().size();i++){
 					if(mapa.getListaPois().get(i).tieneLaClave(nombreLocal)){
 						tienePOI=true;
@@ -53,13 +55,14 @@ public class ActualizacionLocalComercial extends ProcesoAsincronico{
 					usuario.agregarPOI(localModificado);
 				}
 			}
-			this.resultadoOK();
+			resultado.resultadoOK();
 		}
 		catch(Exception e){
 			e.printStackTrace();
+			resultado.resultadoError(e.toString());
 		}
 	}
-	
+
 	public List<String> leerArchivo() throws IOException{
 		String linea=""; 
 		List<String> actualizaciones=new ArrayList<String>();
@@ -78,19 +81,7 @@ public class ActualizacionLocalComercial extends ProcesoAsincronico{
 		return actualizaciones;  
 	}
 
-	@Override
-	public Integer desplegarConsola(User usuario, String terminal_ID,Scanner teclado) {
-		usuario.actualizarLocalComercial(terminal_ID);
-		return null;
-	}
 
-	@Override
-	public void mostrarOpcion() {
-		System.out.println("-->	ACTUALIZAR LOCALES COMERCIALES");
-	}
-
-
-	@Override
 	public void pedirInfo() {
 		System.out.println("---------------------------------------");
 		System.out.println("	ACTUALIZACION DE LOCALES COMERCIALES");
@@ -98,16 +89,15 @@ public class ActualizacionLocalComercial extends ProcesoAsincronico{
 		System.out.println();
 	}
 
-	@Override
-	public void setTask() {
+	public void setTask(TimerTask task,Calendar fechaInicio,User usuario,String terminalID) {
 		ActualizacionLocalComercial auxiliar= new ActualizacionLocalComercial();
 		auxiliar.usuario=usuario;
-		auxiliar.fechaInicio=Calendar.getInstance();
-		task = (TimerTask) auxiliar;
+		fechaInicio=Calendar.getInstance();
+		auxiliar.fechaInicio=fechaInicio;
+		task= auxiliar;
 		
 	}
 
-	@Override
 	public String setProceso() {
 		return "Actualizacion Local Comercial";
 	}
--- src/main/java/dds/grupo3/ProcesosAsincronicos/AgregarAcciones.java
+++ src/main/java/dds/grupo3/ProcesosAsincronicos/AgregarAcciones.java
@@ -9,29 +9,29 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.TimerTask;
 
-import dds.grupo3.Interfaces.Funcionalidad;
-import dds.grupo3.Interfaces.ProcesoAsincronico;
+import dds.grupo3.Interfaces.CommandProcesos;
 import dds.grupo3.Interfaces.User;
 
 
-public class AgregarAcciones extends ProcesoAsincronico implements Funcionalidad{
+public class AgregarAcciones extends TimerTask implements CommandProcesos{
 
 	public String permisosNuevos="";
 	public String respuesta ="";
+	private Calendar fechaInicio;
 	
-	@Override
 	public void run(){
+		ResultadosProcesos resultado=new ResultadosProcesos(this,this.fechaInicio);
 		if(respuesta.equals("N")){
 			try {
 				escribirArchivo();
-				this.resultadoOK();
+				resultado.resultadoOK();
 			} catch (IOException e) {
 				e.printStackTrace();
-				this.resultadoError(e.toString());
+				resultado.resultadoError(e.toString());
 			}
 		}
 	}
-	
+
 	public void escribirArchivo() throws IOException{
 		List<String> newLines = new ArrayList<>();
 		for (String linea : Files.readAllLines(Paths.get("Rol.properties"), StandardCharsets.UTF_8)) {
@@ -40,18 +40,6 @@ public class AgregarAcciones extends ProcesoAsincronico implements Funcionalidad
 		Files.write(Paths.get("Rol.properties"), newLines, StandardCharsets.UTF_8);
 	}
 
-	@Override
-	public Integer desplegarConsola(User usuario, String terminal_ID,Scanner teclado) {
-		usuario.agregarAcciones(terminal_ID);
-		return null;
-	}
-
-	@Override
-	public void mostrarOpcion() {
-		System.out.println("	AGREGAR ACCIONES A USUARIOS");
-	}
-
-	@Override
 	public void pedirInfo() {
 		@SuppressWarnings("resource")
 		Scanner teclado = new Scanner(System.in);
@@ -65,15 +53,16 @@ public class AgregarAcciones extends ProcesoAsincronico implements Funcionalidad
 		this.respuesta=teclado.nextLine();
 		System.out.println();
 	}
-	@Override
-	public void setTask() {
+	
+	public void setTask(TimerTask task,Calendar fechaInicio,User usuario,String terminalID) {
 		AgregarAcciones a = new AgregarAcciones();
-		a.permisosNuevos = permisosNuevos;
-		a.respuesta = respuesta;
-		a.fechaInicio=Calendar.getInstance();
-		this.task = (TimerTask) a;
+		a.permisosNuevos = this.permisosNuevos;
+		a.respuesta = this.respuesta;
+		fechaInicio=Calendar.getInstance();
+		a.fechaInicio=fechaInicio;
+		task=(TimerTask)a;
 	}
-	@Override
+	
 	public String setProceso() {
 		return "Agregar Acciones a Usuarios";
 	}
