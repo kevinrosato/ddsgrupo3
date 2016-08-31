df26011e1effe84c2962a5693ef334227d29770c
kevinrosato <utnkevinrosato@gmail.com>
2016-08-31 01:56:03 -0300

    Pequeño cambio a menuprincipal, mvc acciones casi terminado

--- src/main/java/dds/grupo3/Control/ControllerAccionConsulta.java
+++ src/main/java/dds/grupo3/Control/ControllerAccionConsulta.java
@@ -10,6 +10,21 @@ import dds.grupo3.User.Usuario;
 public class ControllerAccionConsulta {
 	public ModelAndView show( Request request, Response response,Usuario usuario) {
 		HashMap<String, Object> viewModel = new HashMap<>();
-		return new ModelAndView(viewModel, "acciones.html");
+		if(request.queryParams().isEmpty())	return new ModelAndView(viewModel, "acciones.html");
+		String log= request.queryParams("log");
+		if (log==null) log="";
+		String totalFecha=request.queryParams("totalFecha");
+		if (totalFecha==null) totalFecha="";
+		String totalUsuario=request.queryParams("totalUsuario");
+		if (totalUsuario==null) totalUsuario="";
+		Boolean boolLog=false, boolFecha=false, boolUsuario=false;
+		if (log.contains("2"))	boolLog=true;
+		if (totalFecha.contains("true")) boolFecha=true;
+		
+		if (totalUsuario.contains("true"))	boolUsuario=true;
+		
+		System.out.println("log="+boolLog+" totalFecha="+boolFecha+" totalUsuario="+ boolUsuario);
+		
+		return new ModelAndView(viewModel,"acciones.html");
 	}
 }
